a9f8f01b7a98f63a93f9f951b14aa5a3a6cb644e
Zewin <01200361136r@gmail.com>
2023-04-12 05:40:13 +0300

    animation splash screen

--- lib/Feature/view/widgets/splash_view_bady.dart
+++ lib/Feature/view/widgets/splash_view_bady.dart
@@ -1,10 +1,41 @@
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/cupertino.dart';
-
 import 'package:flutter/src/widgets/framework.dart';
 
-class SplashViewBady extends StatelessWidget {
-  const SplashViewBady({super.key});
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+    // slidingAnimation.addListener(() {
+    //   setState(() {
+    //
+    //   });
+    // });
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,6 +44,21 @@ class SplashViewBady extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AssetsData.logo),
+        const SizedBox(
+          height: 10,
+        ),
+        AnimatedBuilder(
+          animation: slidingAnimation,
+          builder: (context,_) {
+            return SlideTransition(
+              position: slidingAnimation,
+              child: const Text(
+                'Read Free Books',
+                textAlign: TextAlign.center,
+              ),
+            );
+          }
+        ),
       ],
     );
   }
